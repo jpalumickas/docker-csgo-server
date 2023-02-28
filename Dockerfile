15fb28d5fdc4e6aeaa20a534fe7db45acbc29717
Justas Palumickas <jpalumickas@gmail.com>
2023-02-28 10:34:53 +0200

    Some updates

--- Dockerfile
+++ Dockerfile
@@ -1,44 +1,43 @@
 FROM ubuntu:18.04
 MAINTAINER Justas Palumickas <jpalumickas@gmail.com>
 
-ENV USER csgo
-ENV HOME /home/$USER
-ENV SERVER $HOME/csgo-server
+ENV TERM xterm
+
+ENV USER steam
+ENV STEAM_DIR /home/$USER
+ENV STEAMCMD_DIR $STEAM_DIR/steamcmd
+ENV SERVER_DIR $STEAM_DIR/csgo-server
+
+ARG STEAMCMD_URL=http://media.steampowered.com/client/steamcmd_linux.tar.gz
 
 RUN apt-get -y update \
     && apt-get -y upgrade \
-    && apt-get -y install lib32gcc1 curl net-tools lib32stdc++6 locales \
+    && apt-get -y install --no-install-recommends --no-install-suggests \
+    lib32gcc1 curl net-tools lib32stdc++6 locales ca-certificates \
     && locale-gen en_US.UTF-8 \
     && update-locale LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8 \
     && dpkg-reconfigure --frontend=noninteractive locales \
     && apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* \
-    && useradd -m $USER \
-    && mkdir $HOME/steamcmd \
-    && mkdir $SERVER \
-    && chown -R $USER:$USER $HOME
+    && adduser --disabled-password --gecos "" $USER \
+    && mkdir $STEAMCMD_DIR \
+    && mkdir $SERVER_DIR \
+    && curl $STEAMCMD_URL | tar -C $STEAMCMD_DIR -xvz \
+    && mkdir -p $STEAM_DIR/.steam/sdk32 \
+    && chown -R $USER:$USER $STEAM_DIR \
+    && chown -R $USER:$USER $SERVER_DIR \
+    && ln -s $STEAMCMD_DIR/linux32/steamclient.so $STEAM_DIR/.steam/sdk32/steamclient.so
 
 ENV LC_ALL en_US.UTF-8
 ENV LANG en_US.UTF-8
 ENV LANGUAGE en_US.UTF-8
 
-COPY csgo_update.txt $SERVER/csgo_update.txt
-COPY steam_appid.txt $SERVER/steam_appid.txt
-
-RUN chown -R $USER:$USER $SERVER
-
 USER $USER
-RUN curl http://media.steampowered.com/client/steamcmd_linux.tar.gz | tar -C $HOME/steamcmd -xvz \
-    && mkdir -p $HOME/.steam/sdk32 \
-    && chown -R $USER:$USER $HOME/.steam \
-    && ln -s $HOME/steamcmd/linux32/steamclient.so $HOME/.steam/sdk32/steamclient.so \
-    && $HOME/steamcmd/steamcmd.sh +runscript $SERVER/csgo_update.txt
 
-COPY run.sh $SERVER/run.sh
+COPY --chown=steam:steam csgo_update.txt $STEAM_DIR/csgo_update.txt
+COPY --chown=steam:steam steam_appid.txt $SERVER_DIR/steam_appid.txt
+COPY --chown=steam:steam run.sh $STEAM_DIR/run.sh
 
-ENV SRCDS_RCON_PASSWORD=my-super-password
+# EXPOSE 27015 27015/udp 27005/udp 27020 27020/udp
 
-EXPOSE 27015 27015/udp 27005/udp 27020/udp
-
-WORKDIR $SERVER
-ENTRYPOINT ["./run.sh"]
-CMD ["-console" "-usercon" "+game_type" "0" "+game_mode" "1" "+mapgroup" "mg_active" "+map" "de_dust2", "+rcon_password", "$SRCDS_RCON_PASSWORD"]
+WORKDIR $SERVER_DIR
+ENTRYPOINT exec ${STEAM_DIR}/run.sh
--- run.sh
+++ run.sh
@@ -1,4 +1,89 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-cd $HOME/csgo-server
-./srcds_run -game csgo -tickrate 128 -autoupdate -steam_dir ~/home/csgo/steamcmd -steamcmd_script ~/home/csgo/csgo/csgo_update.txt $@
+set -e
+
+# These envvars should've been set by the Dockerfile
+# If they're not set then something went wrong during the build
+: "${STEAM_DIR:?'ERROR: STEAM_DIR IS NOT SET!'}"
+: "${STEAMCMD_DIR:?'ERROR: STEAMCMD_DIR IS NOT SET!'}"
+: "${SERVER_DIR:?'ERROR: SERVER_DIR IS NOT SET!'}"
+
+export SERVER_HOSTNAME="${SERVER_HOSTNAME:-Counter-Strike: Global Offensive Dedicated Server}"
+export SERVER_PASSWORD="${SERVER_PASSWORD:-}"
+export RCON_PASSWORD="${RCON_PASSWORD:-changeme}"
+export STEAM_ACCOUNT="${STEAM_ACCOUNT:-changeme}"
+export AUTHKEY="${AUTHKEY:-changeme}"
+export IP="${IP:-0.0.0.0}"
+export PORT="${PORT:-27015}"
+export TV_PORT="${TV_PORT:-27020}"
+export TICKRATE="${TICKRATE:-128}"
+export FPS_MAX="${FPS_MAX:-300}"
+export GAME_TYPE="${GAME_TYPE:-0}"
+export GAME_MODE="${GAME_MODE:-1}"
+export MAP="${MAP:-de_dust2}"
+export MAPGROUP="${MAPGROUP:-mg_active}"
+export MAXPLAYERS="${MAXPLAYERS:-12}"
+export TV_ENABLE="${TV_ENABLE:-1}"
+export LAN="${LAN:-0}"
+export SOURCEMOD_ADMINS="${SOURCEMOD_ADMINS:-}"
+export RETAKES="${RETAKES:-0}"
+
+echo "Updating server5"
+
+# Attempt to update CSGO before starting the server
+# "$STEAMCMD_DIR/steamcmd.sh" +runscript "$STEAM_DIR/csgo_update.txt"
+
+
+echo "Adding config files"
+# Create dynamic autoexec config
+cat << AUTOEXECCFG > "$SERVER_DIR/csgo/cfg/autoexec.cfg"
+log on
+hostname "$SERVER_HOSTNAME"
+rcon_password "$RCON_PASSWORD"
+sv_password "$SERVER_PASSWORD"
+sv_cheats 0
+exec banned_user.cfg
+exec banned_ip.cfg
+AUTOEXECCFG
+
+# Create dynamic server config
+cat << SERVERCFG > "$SERVER_DIR/csgo/cfg/server.cfg"
+tv_enable $TV_ENABLE
+tv_delaymapchange 1
+tv_delay 30
+tv_deltacache 2
+tv_dispatchmode 1
+tv_maxclients 10
+tv_maxrate 0
+tv_overridemaster 0
+tv_relayvoice 1
+tv_snapshotrate 64
+tv_timeout 60
+tv_transmitall 1
+writeid
+writeip
+SERVERCFG
+
+# Start the server
+exec "$BASH" "$SERVER_DIR/srcds_run" \
+        -console \
+        -usercon \
+        -game csgo \
+        -autoupdate \
+        -authkey "$AUTHKEY" \
+        -steam_dir "$STEAMCMD_DIR" \
+        -steamcmd_script "$STEAM_DIR/csgo_update.txt" \
+        -tickrate "$TICKRATE" \
+        -port "$PORT" \
+        -maxplayers_override "$MAXPLAYERS" \
+        +fps_max "$FPS_MAX" \
+        +game_type "$GAME_TYPE" \
+        +game_mode "$GAME_MODE" \
+        +mapgroup "$MAPGROUP" \
+        +map "$MAP" \
+        +sv_setsteamaccount "$STEAM_ACCOUNT" \
+        +tv_port "$TV_PORT" \
+        +ip 0.0.0.0 \
+        +net_public_adr 0.0.0.0 \
+        +clientport 27015 \
+        +sv_lan 1
